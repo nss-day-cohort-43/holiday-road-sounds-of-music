3943fc7315ac0712ecdaf67603e784f409dcfc08
hrissolo <hrissolo@gmail.com>
2020-09-10 11:18:00 -0500

    got the eatery select drop down working. posting the selected eatery to the itinerary preview

--- scripts/eateries/EaterySelect.js
+++ scripts/eateries/EaterySelect.js
@@ -1,26 +1,45 @@
-import { useEateries, getEateries } from "./EateryProvider.js"
+import { useEateries, getEateries } from "./EateryProvider.js";
 
 const eventHub = document.querySelector("main")
 const contentTarget = document.querySelector(".filters__eateries")
+
+
+eventHub.addEventListener("change", event => {
+    if (event.target.id === "eaterySelect") {
+    
+        const customEvent = new CustomEvent("eateryChosen", {
+            detail: {
+                eateryThatWasChosen: event.target.value
+            }
+        })
+        console.log(customEvent)
+        eventHub.dispatchEvent(customEvent)
+
+    }
+}) 
+
+
 
 export const EaterySelect = () => {
     getEateries()
     .then(() => {
     const eatery = useEateries(); 
-    console.log(eatery)
-    
+    console.log(eatery) 
+    render(eatery)
+})
+}
 
-    eventHub.addEventListener("change", event => {
-        if (event.target.id === "eaterySelect") {
-        
-            const customEvent = new CustomEvent("eateryChosen", {
-                detail: {
-                    eateryThatWasChosen: event.target.value
+
+const render = (eateryCollection) => {
+        contentTarget.innerHTML = `
+            <select class="dropdown" id="eaterySelect">
+                <option value="0">select an eatery!</option>
+                ${
+                    eateryCollection.map(eateryObj => {
+                        const eatery = eateryObj.businessName
+                        return `<option value="${eateryObj.businessName}">${eatery}</option>`
+                    })
                 }
-            })
-            console.log(customEvent)
-            eventHub.dispatchEvent(customEvent)
-    
-        }
-    }) }
-    
+            </select>
+        `    
+}
